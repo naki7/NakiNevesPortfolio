7fbf6da51b65860e9f1c53d464f31a2ef481f72a
Joshua Neves <joshie.cad@gmail.com>
2025-01-06 12:08:47 +0000

    Focused on Home Page design
    - removed app.css stylings that conflicted with new stylings
    - added the 2 primary fonts to be used
    - add some root stylings for site to be consistent
    - resized profile img, h1 text, etc on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './home.css';
 
 export default function Home() {
   return (
     <div className='Home'>
-      <h1>Hi! I'm Joshua Neves, but you can just call me Naki!</h1>
+      <h1>Hi! I'm Joshua Neves, but you can just call me <span>Naki!</span></h1>
       <p>I'm a hardworking individual with a love for Front-End Engineering.<span>And a tiny obsession with otters!</span></p>
       {/* add otter svgs */}
       <img className='ProfileImg' src='Profile.png' alt='Profile'/>
-      {/* add logic for buttons to change state in app */}
+      {/* add logic for buttons to change state in app and pull in as prop*/}
       <button>About Me</button>
       <button>Portfolio</button>
       <button>Contact Me</button>
