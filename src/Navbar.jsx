ff847a9140354e55b29dbb4e2b2cdca127933027
Joshua Neves <joshie.cad@gmail.com>
2025-01-10 12:18:47 +0000

    Navbar phone burger menu functionality
    - added icons for opening and closing the navbar
    -added usestate variables to decide when the navbar should be open/closed and to only show the navbar list items when it is set to open
    - added stylings to improve navbar based on this
    - started working on an animation keyframe to play when navbar opens and closes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
 export default function Navbar(props) {
   const {currPage, UpdatePage} = props;
 
+  const [opener, setOpener] = useState(true);
+
+  const updateOpeners = () => {
+    return setOpener(!opener);
+  }
+
   return (
     <div className='navbar'>
       {/* add plus icon && burger menu icon with functionality to open and close navbar on small menus */}
-      <ul>
+      <div className='navOpeners'>
+        {opener && <i className="fa-solid fa-bars fa-2x" onClick={updateOpeners}></i>}
+        {!opener && <i className="fa-solid fa-xmark fa-2x" onClick={updateOpeners}></i>}
+      </div>
+      {!opener && <ul>
         <li className='navItem' onClick={()=>{UpdatePage('home')}}>Home</li>
         {(currPage != 'aboutMe') ?  <li className='navItem' onClick={()=>{UpdatePage('aboutMe')}}>About Me</li> : <li className='navItem' onClick={()=>{UpdatePage('portfolio')}}>Portfolio</li>  }
         {(currPage != 'portfolio' && currPage != 'aboutMe') ?  <li className='navItem' onClick={()=>{UpdatePage('portfolio')}}>Portfolio</li> : <li className='navItem' onClick={()=>{UpdatePage('contact')}}>Contact Me</li> }
-      </ul>
+      </ul>}
     </div>
   )
 }
