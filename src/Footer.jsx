92e9a2c401282922f8f9b4ef85c602936d423f7b
Joshua Neves <joshie.cad@gmail.com>
2025-01-14 17:48:00 +0000

    Styled and added function to Footer
    - added updated CV to public folder
    - added appropriate links to icons for CV, LinkiedIn, and GitHub
    - added styling to footer background, icons, text and line break
    - added styling so that the footer is always at the bottom of the page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
 
 export default function Footer() {
   return (
     <div className='Footer'>
-        <p>footer</p>
-        {/* add links to CV, LinkedIn & GitHub, with Icons from FontAwesome */}
-        <i className="fa-brands fa-linkedin"></i><i className="fa-brands fa-square-github"></i><i className="fa-solid fa-file"></i>
-        <p>Copyright © 2025 Joshua Neves. All rights reserved.</p>
+        <div className='footline'/>
+        <div className='footLinks'>
+          <a href='https://www.linkedin.com/in/joshua-neves-54baa6196/' target='_blank' title='LinkedIn'><i className="fa-brands fa-linkedin fa-2x"></i></a>
+          <a href='https://github.com/naki7' target='_blank' title='GitHub'><i className="fa-brands fa-square-github fa-2x"></i></a>
+          <a href='/CVJoshuaNeves2025.pdf' target='_blank' title='CV'><i className="fa-solid fa-file fa-2x"></i></a>
+        </div>
+        <p className='footText'>Copyright © 2025 Joshua Neves. All rights reserved.</p>
     </div>
   )
 }
