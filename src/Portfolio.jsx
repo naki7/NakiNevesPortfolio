9cc0367cd67e011e46b57c5b310c5d5688519e46
Joshua Neves <joshie.cad@gmail.com>
2025-01-28 03:38:38 +0000

    added a portfoliohandler component to the portfolio component
    - this checks if the coding import exists (it does since it is imported), it then breaks down the object into a smaller level object, then loops over the object inside to push each element with the appropriate values to the coding array. (might need to change this in the future as it currently only does the first key since there is only 1 to portfolio piece in the object)
    -the coding array is then pushed into the main portfolio array
    - check if art exists (which it does since it is also imported), it then breaks down into the first level of the object, then it loops over each key before then going and breaking down into each key and then again it goes through each value in the object to return the appropriate input, it then pushes it within the appropriate elements into the main art array
    - the art array is then push into the main portfolio array
    
    (might make changes to the portfoliohandler component later as there is currently a lot of reused code that could be cut down on. calling the portfolio handler twice and passing in either coding or art into each but having the component not check if it is art/coding but rather if the final object has a length of 4 or 6 might be better as the steps before the switches are similar so could rather make those two separate if statements inside the same for loop)
    
    - styling the portfolio page would be the next important step

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,19 +1,67 @@
-import React from 'react'
+import React from 'react';
+import './portfolio.css';
+import { Coding, Art } from './portfolioObjects.js';
 
 export default function Portfolio() {
 
-  // create a function that loops over an array of titles that will pair them with an image and a description to create individual protfolio project cards
+  const PortfolioHandler = () =>{
+    let entirePortfolio = [];
+
+    if(Coding){
+      let codePortfolio = [];
+      const codingKeys = Coding[Object.keys(Coding)[0]];
+      // console.log(Object.keys(codingKeys).length);
+      for(let i = 0; i < Object.keys(codingKeys).length; i++){
+        // console.log(Object.values(codingKeys)[i]);
+        let currentValue = Object.values(codingKeys)[i];
+        switch(i){
+          case 0:
+            codePortfolio.push(<h3>{currentValue}</h3>);
+            break;
+          case 1:
+            codePortfolio.push(<img src={currentValue} alt={Object.values(codingKeys)[0]}/>);
+            break;
+          case 2:
+            codePortfolio.push(<p>{currentValue}</p>);
+            break;
+            case 3:
+              codePortfolio.push(<a href={currentValue} target='_blank'><i className="fa-solid fa-up-right-from-square"></i></a>);
+              break;
+        }
+      }
+      entirePortfolio.push(<div className='codingDiv'>{codePortfolio}</div>);
+    }
+    if(Art){
+      let artPortfolio = []
+      console.log(Object.keys(Art).length)
+      for(let i = 0; i < Object.keys(Art).length; i++){
+        const artKeys = Art[Object.keys(Art)[i]]
+        console.log(artKeys)
+        for(let j = 0; j < Object.keys(artKeys).length; j++){
+          let currentValue = Object.values(artKeys)[j];
+          console.log( i, j, currentValue, Object.keys(artKeys))
+          switch(j){
+            case 0:
+              artPortfolio.push(<h3>{currentValue}</h3>);
+              break;
+            case 1:
+            case 2:
+            case 4:
+            case 5:
+              artPortfolio.push(<img src={currentValue} alt={Object.values(artKeys)[0]}/>);
+              break;
+            case 3:
+              artPortfolio.push(<p>{currentValue}</p>);
+              break;
+          }
+        }
+      }
+      entirePortfolio.push(<div className='artDiv'>{artPortfolio}</div>);
+    }
+    return(<div className='Portfolio'>{entirePortfolio}</div>)
+  }
 
   return (
-    <div className='Portfolio'>
-      <div className='DevProjects'>
-        <h3>Dev Projects</h3>
-        {/* add webdevelopment projects here */}
-      </div>
-      <div className='ArtProjects'>
-      <h3>Art Projects</h3>
-        {/* add art projects here */}
-      </div>
-    </div>
+    <PortfolioHandler/>
   )
 }
