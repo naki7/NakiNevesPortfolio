1d20740770c9872ec5c498feb79e6bf32b6d1925
Joshua Neves <joshie.cad@gmail.com>
2025-01-28 03:28:00 +0000

    Create portfolioObjects.js
    - added a javascript file that contains all the portfolio details within objects, one for art and one for coding
    - both objects are then exported

---
+++ src/portfolioObjects.js
@@ -0,0 +1,50 @@
+
+
+const Coding = {
+    nakisSnakis : {
+        title: `Naki's Snakis`,
+        imgSrc: '/WebDevProjects/NakisSnakisImg.jpg',
+        description: 'The goal of this website was to create the front view of an artificial online macaroon store. Within this project I tried to purposefully challenge myself both to create a site that was visually appealing while also having the common functionalities that would be required of site like this. I also tried to push myself by using react states that I did not have much prior experience with, in order to create a site that could keep track of how many items were added to the cart, what flavors these items were, and how much they cost. The final functional challenge was taking all of that information to display it on a modal screen that displayed the title, allowed the quantities to be changed in a different component, as well as having the functionality of purchasing or clearing the cart without having to reload the site. The last part that I did not expect to be so difficult, but I still learnt a lot through the process, was using media queries in order to make the website fully scalable for small phone screens, to the much larger computer screens.',
+        url: 'https://nakis-snakis.netlify.app/',
+    },
+}
+
+const Art = {
+    GraphicDesign : {
+        title: `Crystalline Rose, Beauty Product & Communication Design`,
+        imgSrc1: '/ArtProjects/CrystallineRoseLogo.jpg',
+        imgSrc2: '/ArtProjects/CrystallineRoseEmail.jpg',
+        description: 'As a part of a graphic design training program, my final task was to create a full scale product brand based on a previous project I worked on. I had originally made the logo, but I reworked it to be more readable, I also made the template for a possible email, templates for Instagram posts, and labels for the possible products.',
+        imgSrc3: '/ArtProjects/CrystallineRoseInstagramTemplates.jpg',
+        imgSrc4: '/ArtProjects/CrystallineRoseProducts.jpg',
+    },
+    PersonalProjects : {
+        title: `Assortment of Personal Projects`,
+        imgSrc1: '/ArtProjects/TattooIdea.jpg',
+        imgSrc2: '/ArtProjects/OtteroLogo.jpg',
+        description: 'Over the years I have collected a group of various small projects that I did for myself as a part of self expression and allowing what is in my mind to exist in the world. I have made a tattoo design based heavily on the Fourth Wing by Rebecca Yarros book covers, a simple otter design, a recreation of a photo of a panther in a zoo, and a styalized version of two otters holding hands.',
+        imgSrc3: '/ArtProjects/BigKitty.jpg',
+        imgSrc4: '/ArtProjects/OttersHoldingHands.png',
+    },
+    Gifts : {
+        title: `Various Art Pieces I've Made as Gifts`,
+        imgSrc1: '/ArtProjects/Nephew.jpg',
+        imgSrc2: '/ArtProjects/SisterInLawsDogs.jpg',
+        description: 'Part of showing love to others for me is to create something that is attached to the person the I see as having emotional & passion related meaning to them. I have made a digital painting of my nephew for my mom, 2 different images for each of my sister-in-laws, and a pokemon trainer related drawing for a friend.',
+        imgSrc3: '/ArtProjects/SisterInLawsBeagle.jpg',
+        imgSrc4: '/ArtProjects/PokemonTrainers.jpg',
+    },
+    Pokemon : {
+        title: `Collection of Pokemon Art I've Made`,
+        imgSrc1: '/ArtProjects/Shedinja.jpg',
+        imgSrc2: '/ArtProjects/MudkipDrawing.jpg',
+        description: 'Pokemon is a hyperfixation that I have repeatedly returned to over the years, and in some moments I have felt inspired to create art of them. From a creepy shedinja, to my favourite pokemon - mudkip, Rhytoise which is a fusion between blastoise and rhydon, and lastly an imagining of both a ninjask and shedinja appearing after a nincada evolves.',
+        imgSrc3: '/ArtProjects/Rhytoise.jpg',
+        imgSrc4: '/ArtProjects/NinjaskShedinja.jpg',
+    },
+}
+
+export {
+    Coding,
+    Art
+}
