02c31ca78271ffedafe1c8e6d02d8684a0e9c4b0
Joshua Neves <joshie.cad@gmail.com>
2025-02-04 12:52:31 +0000

    Portfolio Page styling implemented
    - created 2 useStates, one to control which section is currently chosen between Coding and Art, and the other to control which project is currently chosen, such as Naki's Snakis, or the graphic design projects, etc.
    - added 2 state handlers for each of the above with appropriate code so they would function onclick which was added to titles and headers
    - implemented styles that would only be applied when those states are used through conditionally rendered classnames
    - any projects that do not have a certain class, as well as any project titles that do not have a certain class were set to hidden and display as none.
    - classes being added then make these render along with animations for them to fade in
    - a scroll to top was added to the art header on click as it sometimes when clicked would be pushed above the screen view
    - added chevron icons to titles and headers along with animations and conditional classes to make them look more clickable
    - seperated the art images pairs into divs so that they could be given spacing between each other
    
    - changed the hero section headers on the aboutme and portfolio pages. changed them back to the black :root colour as it felt like it makes them stand out more from the rest of the page that way and matches the home page too

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './portfolio.css';
 import { Coding, Art } from './portfolioObjects.js';
 
 export default function Portfolio() {
+
+  const [section, setSection] = useState(null);
+  const [project, setProject] = useState(null);
+
+  const sectionHandler = (title) =>{
+    if(!title){return}
+    setProject(null);
+    if(title === 'coding' && section != 'coding'){
+      setSection('coding');
+    }else if(title === 'art' && section != 'art'){
+      window.scrollTo(0, 420);
+      setSection('art');
+    }else{
+      setSection(null);
+    }
+  }
+
+  const projectHandler = (name) =>{
+    console.log(name);
+    if(!name){return}
+    if(project != name){
+      setProject(name);
+    } else{
+      setProject(null);
+    }
+    console.log(project);
+  }
+
+
 
   const PortfolioSectionCreator = (obj) =>{
     for(let i = 0; i < Object.keys(obj).length; i++){
@@ -13,10 +42,15 @@ export default function Portfolio() {
         const codingElement = Object.values(arrValues).map((value, index) =>{
           return(
             <div className='codingDiv' key={index}>
-              <h3 className='title'>{Object.values(value)[0]}</h3>
-              <a href={Object.values(value)[3]} target='_blank' title={Object.values(value)[0]}><img src={Object.values(value)[1]} alt={Object.values(value)[0]}/></a>
-              <p className='description'>{Object.values(value)[2]}</p>
-              <a href={Object.values(value)[3]} target='_blank' title={Object.values(value)[0]}><div className='websiteLink'><p>Visit Me</p><i className='fa-solid fa-up-right-from-square'></i></div></a>
+              <div className={'title' + ((project === Object.values(value)[0]) ? ' highlight' : ' ')}>
+                <h3 onClick={() =>{projectHandler(Object.values(value)[0])}}>{Object.values(value)[0]}</h3>
+                <i className={"chevron fa-solid fa-chevron-right" + ((project === Object.values(value)[0]) ? ' chevronActive' : ' ')}></i>
+              </div>
+              <div className={(project === Object.values(value)[0]) ? ' projectShow' : ' projectHidden'}>
+                <a href={Object.values(value)[3]} target='_blank' title={Object.values(value)[0]}><img src={Object.values(value)[1]} alt={Object.values(value)[0]}/></a>
+                <p className='description'>{Object.values(value)[2]}</p>
+                <a className='websiteAnchor' href={Object.values(value)[3]} target='_blank' title={Object.values(value)[0]}><div className='websiteLink'><p>Visit Me</p><i className='fa-solid fa-up-right-from-square'></i></div></a>
+              </div>
             </div>
           )
         })
@@ -27,12 +61,21 @@ export default function Portfolio() {
         const artElement = Object.values(objKeys).map((value, index) =>{
           return(
             <div className='artDiv' key={index}>
-              <h3 className='title'>{Object.values(value)[0]}</h3>
-              <img src={Object.values(value)[1]} alt={Object.values(value)[0]}/>
-              <img src={Object.values(value)[2]} alt={Object.values(value)[0]}/>
-              <p className='description'>{Object.values(value)[3]}</p>
-              <img src={Object.values(value)[4]} alt={Object.values(value)[0]}/>
-              <img src={Object.values(value)[5]} alt={Object.values(value)[0]}/>
+              <div className={'title' + ((project === Object.values(value)[0]) ? ' highlight' : ' ')}>
+                <h3 onClick={() =>{projectHandler(Object.values(value)[0])}}>{Object.values(value)[0]}</h3>
+                <i className={"chevron fa-solid fa-chevron-right" + ((project === Object.values(value)[0]) ? ' chevronActive' : ' ')}></i>
+              </div>
+              <div className={(project === Object.values(value)[0]) ? ' projectShow' : ' projectHidden'}>
+                <div className='imgPair'>
+                  <img src={Object.values(value)[1]} alt={Object.values(value)[0]}/>
+                  <img src={Object.values(value)[2]} alt={Object.values(value)[0]}/>
+                </div>
+                <p className='description'>{Object.values(value)[3]}</p>
+                <div className='imgPair'>
+                  <img src={Object.values(value)[4]} alt={Object.values(value)[0]}/>
+                  <img src={Object.values(value)[5]} alt={Object.values(value)[0]}/>
+                </div>
+              </div>
             </div>
           )
         })
@@ -49,12 +92,22 @@ export default function Portfolio() {
         <p>These are a few selected coding & art projects I've done over the years</p>
       </div>
       <div className='codingComponent'>
-        <h2>Coding</h2>
-        <PortfolioSectionCreator Coding={Coding}/>
+        <div className={'header' + ((section === 'coding') ? ' sectionHighlight' : ' ')} onClick={() => {sectionHandler('coding')}}>
+          <h2>Coding</h2>
+          <i className={"chevron fa-solid fa-chevron-right" + ((section === 'coding') ? ' chevronActive' : ' ')}></i>
+        </div>
+        <div className={(section === 'coding') ? ' sectionShow' : ' sectionHidden'}>
+          <PortfolioSectionCreator Coding={Coding}/>
+        </div>
       </div>
       <div className='artComponent'>
-        <h2>Art</h2>
-        <PortfolioSectionCreator Art={Art}/>
+        <div className={'header' + ((section === 'art') ? ' sectionHighlight' : ' ')} onClick={() => {sectionHandler('art')}}>
+          <h2>Art</h2>
+          <i className={"chevron fa-solid fa-chevron-right" + ((section === 'art') ? ' chevronActive' : ' ')}></i>
+        </div>
+        <div className={(section === 'art') ? ' sectionShow' : ' sectionHidden'}>
+          <PortfolioSectionCreator Art={Art}/>
+        </div>
       </div>
     </div>
   )
