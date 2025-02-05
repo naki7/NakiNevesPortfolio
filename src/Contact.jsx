cbbbe6907460e0deeefedbd035d9c19a4a9dc819
Joshua Neves <joshie.cad@gmail.com>
2025-02-05 13:33:31 +0000

    Added Contact page functionality
    - added email.js api
    - added send email function that uses email.js to handle the server side functions of the email
    - added a hero section with a header, p tag and img of an otter
    - refined the input fields and labels of the form section
    - styled the hero section
    -started styling the form section
    
    - still need to finish styling form section
    - need to change some functionality with the email function and the input form as at the moment a blank email can be sent without restriction and the user does not have anything to tell them that the email was sent
    - planning on adding a modal component to tell the user that the email has been sent, and at the same time clearing the input fields

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './contact.css';
 
 export default function Contact() {
+  const form = useRef();
 
-  // add useEffect function that uses an API to send email to me
+  const sendEmail = (e) =>{
+    e.preventDefault();
+
+    emailjs.sendForm('contact_service', 'contact_form', form.current, {publicKey: 'wRsX70yMHFy9JN6_a'}).then(() => {
+      console.log('SUCCESS!');
+    },
+    (error) => {
+      console.log('FAILED...', error.text);
+    }
+    )
+  }
+  
 
   return (
-    <div>
-      <form>
-        {/* add on change and value state effects */}
-        <input type='text' placeholder='Name...'  />
-        <input type='text' placeholder='Your Email...'  />
-        <input type='text' placeholder='Message...'  />
-        <input type='submit'/>
+    <div className='contactPage'>
+      <div className='contactHero'>
+        <h2>Feel free to reach out to join the Romp!</h2>
+        <p>Fun fact: a romp is the collective noun for a group of otters. And that is because they play joyfully!</p>
+        <img src='/Otters/Otter1.svg' alt='an otter turning around'/>
+      </div>
+      <form className='contactForm' ref={form} onSubmit={sendEmail}>
+        <div className='inputContainer'>
+          <label>Name</label>
+          <input className='input' type='text' name='user_name' placeholder='Your Name...'  />
+        </div>
+        <div className='inputContainer'>
+          <label>Email</label>
+          <input className='input' type='email' name='user_email' placeholder='Your Email...'  />
+        </div>
+        <label>Message</label>
+        <textarea className='input' name='message' placeholder='Message...'  />
+        <input className='submit' type='submit'/>
       </form>
     </div>
   )
