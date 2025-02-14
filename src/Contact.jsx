cc67fbb677b3fdf15e3e3ae4f59269864ff2556c
Joshua Neves <joshie.cad@gmail.com>
2025-02-14 15:01:07 +0000

    Update Contact.jsx
    fixed emailjs import

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from 'emailjs-com';
 import './contact.css';
 
 export default function Contact() {
